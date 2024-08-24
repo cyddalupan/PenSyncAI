3181ce3a4096c3d950564eb86b7acde60adabd4f
cyd dalupan <cydmdalupan@gmail.com>
2024-08-24 02:24:25 +0000

    Ai Score for writer

--- PenSyncAI/settings.py
+++ PenSyncAI/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&2k7kkgf@$*k9!)s^lnx9t+@%(179swc$hjqv#-2yvc%mh8vev'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- modules/admin.py
+++ modules/admin.py
@@ -3,6 +3,13 @@ from .models import Module, Article
 from ckeditor.widgets import CKEditorWidget
 from django.db import models
 from django.utils.html import format_html
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+
+load_dotenv()
+
+client = OpenAI()
 
 class ArticleInline(admin.TabularInline):
     model = Article
@@ -40,12 +47,10 @@ class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ('score', 'feedback', 'writer', 'created_at', 'updated_at')
 
     def save_model(self, request, obj, form, change):
-        obj.score = 99
-        obj.feedback = "I am Admin"
-        if not request.user.is_superuser and not change:
-            obj.score = 10
-            obj.feedback = "Test"
-        if not change or not obj.writer_id:  # If creating a new article
+        score, suggestion = ai_check_write(obj.content)
+        obj.score = score
+        obj.feedback = suggestion
+        if not change or not obj.writer_id:
             obj.writer = request.user
         super().save_model(request, obj, form, change)
 
@@ -55,3 +60,52 @@ class ArticleAdmin(admin.ModelAdmin):
             return False
         return super().has_change_permission(request, obj)
 
+def ai_check_write(article):
+    messages = [
+        {"role": "system", "content": "Trigger the score_article function no need for reply.  You rate the user article besed on this rules: use capital letter on the title, sound sad."},
+        {"role": "user", "content": article},
+    ]
+
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+            tools=[
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "score_article",
+                        "description": "this function always triggers. this gives the article score and suggestion on how to improve the score",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "score": {
+                                    "type": "integer",
+                                    "description": "Score of the writter on how good it follows the rules from 1 to 100. 100 is perfect.",
+                                },
+                                "suggestion": {
+                                    "type": "string",
+                                    "description": "Suggestion for the writter on how to get a higher score. just congratulate if the score is perfect",
+                                },
+                            },
+                            "required": ["score"],
+                        },
+                    },
+                },
+            ],
+        )
+        tool_calls = completion.choices[0].message.tool_calls
+        
+        if tool_calls:
+            function_name = tool_calls[0].function.name 
+            arguments = tool_calls[0].function.arguments 
+            arguments_dict = json.loads(arguments)
+            
+            if function_name == "score_article":
+                score = arguments_dict['score']
+                suggestion = arguments_dict['suggestion']
+                return score, suggestion
+            else:
+                return None, None
+    except Exception as e:
+        return None, None
